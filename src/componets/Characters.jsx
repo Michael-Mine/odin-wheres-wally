76eb7da3eb7bc0d67ced0f6d1c1c51da3eb03ec0
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-05-27 22:15:52 +0100

    refactor: move Characters to module

---
+++ src/componets/Characters.jsx
@@ -0,0 +1,25 @@
+import charactersImage from "../assets/wheres-waldo-characters.jpg";
+import styles from "../styles/Characters.module.css";
+
+function Characters() {
+  return (
+    <>
+      <div className={styles.charactersContainer}>
+        <img
+          src={charactersImage}
+          alt="characters"
+          className={styles.charactersImage}
+        />
+      </div>
+      <p>
+        <b>
+          Wally &nbsp; &nbsp; Woof &nbsp; &nbsp; &nbsp; &nbsp; Wenda &nbsp;
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Wizard &nbsp; &nbsp;
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Odlaw
+        </b>
+      </p>
+    </>
+  );
+}
+
+export default Characters;
--- src/pages/SpaceStation.jsx
+++ src/pages/SpaceStation.jsx
@@ -1,6 +1,5 @@
-import charactersImage from "../assets/wheres-waldo-characters.jpg";
 import spaceStationImage from "../assets/Wheres-Waldo-Space-Station.jpg";
-import styles from "../styles/SpaceStation.module.css";
+import Characters from "../componets/Characters";
 
 function SpaceStation() {
   const handleImageClick = (e) => {
@@ -21,20 +20,7 @@ function SpaceStation() {
   return (
     <>
       <h2>Space Station</h2>
-      <div className={styles.charactersContainer}>
-        <img
-          src={charactersImage}
-          alt="characters"
-          className={styles.charactersImage}
-        />
-      </div>
-      <p>
-        <b>
-          Wally &nbsp; &nbsp; Woof &nbsp; &nbsp; &nbsp; Wenda &nbsp; &nbsp;
-          &nbsp; &nbsp; &nbsp; &nbsp; Wizard &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-          &nbsp; &nbsp; Odlaw
-        </b>
-      </p>
+      <Characters />
       <img
         src={spaceStationImage}
         alt="space station"
