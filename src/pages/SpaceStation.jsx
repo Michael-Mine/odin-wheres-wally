9f9f8cdb1d01a88dfbf0e02d3d589ba8a4ca5ea0
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-05-27 22:02:12 +0100

    feat: add handle image click to get coordinates

--- src/pages/SpaceStation.jsx
+++ src/pages/SpaceStation.jsx
@@ -3,6 +3,21 @@ import spaceStationImage from "../assets/Wheres-Waldo-Space-Station.jpg";
 import styles from "../styles/SpaceStation.module.css";
 
 function SpaceStation() {
+  const handleImageClick = (e) => {
+    const target = e.target;
+
+    // Get click position relative to image
+    const x = e.pageX - target.offsetLeft;
+    const y = e.pageY - target.offsetTop;
+
+    // Normalize to percentage of width and height
+    // Multiply by 10000 and divide by 100 to keep 2 decimal places
+    const xCoord = Math.floor((x / target.width) * 10000) / 100;
+    const yCoord = Math.floor((y / target.height) * 10000) / 100;
+
+    console.log(`Clicked at: X=${xCoord}%, Y=${yCoord}%`);
+  };
+
   return (
     <>
       <h2>Space Station</h2>
@@ -20,7 +35,11 @@ function SpaceStation() {
           &nbsp; &nbsp; Odlaw
         </b>
       </p>
-      <img src={spaceStationImage} alt="space station" />
+      <img
+        src={spaceStationImage}
+        alt="space station"
+        onClick={handleImageClick}
+      />
     </>
   );
 }
