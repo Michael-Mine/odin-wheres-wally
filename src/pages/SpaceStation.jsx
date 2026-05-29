bbf8c31b5e4f05273ca459cad340be2ef951f590
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-05-29 21:39:12 +0100

    feat: add popover box with names on click

--- src/pages/SpaceStation.jsx
+++ src/pages/SpaceStation.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import spaceStationImage from "../assets/Wheres-Waldo-Space-Station.jpg";
-import Characters from "../componets/Characters";
+import Characters from "../components/Characters";
+import styles from "../styles/SpaceStation.module.css";
 
 function SpaceStation() {
+  const [remainingCharacters, setremainingCharacters] = useState([
+    "Wally",
+    "Woof",
+    "Wendy",
+    "Wizard",
+    "Odlaw",
+  ]);
+  const [coord, setCoord] = useState([0, 0]);
+  console.log("rendering");
+  const box = document.getElementById("selectionBox");
+
   const handleImageClick = (e) => {
     const target = e.target;
 
@@ -15,6 +28,17 @@ function SpaceStation() {
     const yCoord = Math.floor((y / target.height) * 10000) / 100;
 
     console.log(`Clicked at: X=${xCoord}%, Y=${yCoord}%`);
+    setCoord([xCoord, yCoord]);
+
+    box.style.top = e.pageY + "px";
+    box.style.left = e.pageX + "px";
+    box.togglePopover();
+  };
+
+  const handleCharacterSelect = (character) => {
+    box.togglePopover();
+    console.log("click");
+    //POST request with character & coord
   };
 
   return (
@@ -24,8 +48,27 @@ function SpaceStation() {
       <img
         src={spaceStationImage}
         alt="space station"
+        id="spaceStationImage"
         onClick={handleImageClick}
       />
+      <div
+        className={styles.box}
+        id="selectionBox"
+        popover="auto"
+        anchor="spaceStationImage"
+      >
+        <ul>
+          {remainingCharacters.map((character) => {
+            return (
+              <li key={character}>
+                <button onClick={() => handleCharacterSelect(character)}>
+                  {character}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
