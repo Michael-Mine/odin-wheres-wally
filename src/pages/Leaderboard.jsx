2b5ae13ed420266a4eb039725e663533176ed6e4
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-06-30 09:35:34 +0100

    feat: add Leaderboard page

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,11 +1,35 @@
 import { Link } from "react-router";
+import useScores from "../hooks/useScores";
+import formatTime from "../utils/formatTime";
+import styles from "../styles/Leaderboard.module.css";
 
 function Leaderboard() {
-  // fetch data
+  const { scores, scoreError, scoreLoading } = useScores();
+
+  if (scoreLoading) return <h4>Checking Scores</h4>;
+  if (scoreError) return <h4>Error Checking Scores</h4>;
+
   return (
     <>
       <h2>Leaderboard</h2>
-
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        {scores.map((item, index) => {
+          return (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{item.username}</td>
+              <td>{formatTime(item.time)}</td>
+            </tr>
+          );
+        })}
+      </table>
       <Link to="/">
         <button>Home</button>
       </Link>
